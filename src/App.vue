dcdf2e46bf7df6e8a491b6f1ded7b86d7b2738d1
LcrPrograming <83461083+Luiscrd@users.noreply.github.com>
2022-11-25 16:39:51 +0100

    upload

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-
-const counter = ref(0);
 
 </script>
 
 <template>
   <div class="wrapper">
-    <h1>Counter</h1>
-    <p>{{ counter }}</p>
-    <button
-      type="button"
-      class="btn btn-success"
-      @click="counter++"
-      >+1</button>
+    <h1>App.vue</h1>
   </div>
 </template>
 
